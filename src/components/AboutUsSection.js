70ea72f0e0c4c4a026fe0fd8fbe2b5e7ccf8dba5
Amith Raravi <amith.raravi@gmail.com>
2020-08-10 00:51:41 +0200

    Modified About Us page

--- src/components/AboutUsSection.js
+++ src/components/AboutUsSection.js
@@ -2,30 +2,31 @@ import React from 'react';
 import styles from '../styles/AboutUsSection.module.css';
 
 const AboutUsSection = () => (
-  <div className="container">
-    <section className={styles.aboutSection}>
-      <div className={styles.aboutSection_image}>
-        <h2 className={styles.aboutSection_imageTitle}>&apos;Seeing&apos; is believing.</h2>
-      </div>
-      <p className={styles.aboutSection_text}>
-        Technology keeps changing so fast that it’s difficult to keep
-        track of it all. At SPC, we want to make sure that the latest
-        technology is made available to everyone. We promise to deliver
-        products that are tailor-made to the needs of every customer.
-      </p>
-      <p className={styles.aboutSection_text}>
-        Our mission is to remove the distance between people,
-        be it a different continent or just the other part of town.
-      </p>
-      <p className={styles.aboutSection_text}>
-        We provide Interactive displays that can be utilised by the
-        education system, the business room, the corporate office and/or
-        at your home. SPC is all about simplicity, our user-friendly
-        displays will ensure that you can use it however you want to.
-      </p>
-      <a type="button" className={styles.aboutSection_button} href="mailto:">Get in Touch</a>
-    </section>
-  </div>
+  <>
+    <div className={styles.aboutSection_top} />
+    <div className="container">
+      <section className={styles.aboutSection}>
+        <h2 className={styles.aboutSection_title}>&apos;Seeing&apos; is believing</h2>
+        <p className={styles.aboutSection_text}>
+          Technology keeps changing so fast that it’s difficult to keep
+          track of it all. At SPC, we want to make sure that the latest
+          technology is made available to everyone. We promise to deliver
+          products that are tailor-made to the needs of every customer.
+        </p>
+        <p className={styles.aboutSection_text}>
+          Our mission is to remove the distance between people,
+          be it a different continent or just the other part of town.
+        </p>
+        <p className={styles.aboutSection_text}>
+          We provide Interactive displays that can be utilised by the
+          education system, the business room, the corporate office and/or
+          at your home. SPC is all about simplicity, our user-friendly
+          displays will ensure that you can use it however you want to.
+        </p>
+        <a type="button" className={styles.aboutSection_button} href="mailto:">Get in Touch</a>
+      </section>
+    </div>
+  </>
 );
 
 export default AboutUsSection;
